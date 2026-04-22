511a88e1ef47b79851991ba24a508eed7bd076d8
Rainmo0 <aunknown145@gmail.com>
2026-04-22 20:53:31 +0700

    First commit :D

---
+++ basdut_adventure/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for basdut_adventure project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basdut_adventure.settings')
+
+application = get_wsgi_application()
