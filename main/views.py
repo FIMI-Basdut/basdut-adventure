77cf2c8cbe453e57bdbe421830d762781f3bf24f
Acid <Acid-aja@users.noreply.github.com>
2026-04-28 20:15:57 +0700

    feat/ticket:add routing to show ticket html funciton

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def show_mantap(request):
     return render(request, "mantap.html")
+
+def show_ticket(request):
+    return render(request, 'ticket.html')
