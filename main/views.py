1fa0aacbbec4c71304589776ebc53fb0c4e55bbb
Acid <Acid-aja@users.noreply.github.com>
2026-04-30 20:51:00 +0700

    feat/navbar: menambahkan navbar untuk guest, admin, organizer, dan customer

--- main/views.py
+++ main/views.py
@@ -4,3 +4,26 @@ from django.shortcuts import render, redirect
 def show_mantap(request):
     return render(request, "mantap.html")
 
+def preview_navbar_guest(request):
+    return render(request, 'base.html')
+
+def preview_navbar_admin(request):
+    context = {
+        'role': 'admin',
+        'username': 'Administrator'
+    }
+    return render(request, 'base.html', context)
+
+def preview_navbar_organizer(request):
+    context = {
+        'role': 'organizer',
+        'username': 'Andi Organizer'
+    }
+    return render(request, 'base.html', context)
+
+def preview_navbar_customer(request):
+    context = {
+        'role': 'customer',
+        'username': 'Izzudin Abdul Rasyid'
+    }
+    return render(request, 'base.html', context)
