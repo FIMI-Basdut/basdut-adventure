c5621516b8cfe514e06c9220a95592072ad88cd6
Rainmo0 <aunknown145@gmail.com>
2026-05-14 17:31:40 +0700

    [feat] supabase connection, register backend for customer and organizer

--- autentikasi/views.py
+++ autentikasi/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from basdut_adventure.db import get_connection
+
+import uuid
 
 
 def login_user(request):
@@ -29,6 +32,7 @@ def register_form(request):
    role = request.GET.get('role')
 
    valid_roles = ['pelanggan', 'penyelenggara', 'administrator']
+   
    if role not in valid_roles:
       return redirect('autentikasi:register_role_selection')
 
@@ -41,5 +45,127 @@ def register_action(request):
    if request.method == "POST":
       role = request.POST.get('role')
       username = request.POST.get('username')
+      password = request.POST.get('password')
+      user_id = str(uuid.uuid4())
+      
+      # get the tools for query in db
+      conn = get_connection()
+      
+      if role == 'pelanggan':
+         customer_id = str(uuid.uuid4())
+         full_name = request.POST.get('nama')
+         nomor_telepon = request.POST.get('nomor_telepon')
+         
+         try:
+            with conn.cursor() as cur:
+                  # get role id based on customer
+                  cur.execute(
+                     """
+                     SELECT role_id
+                     FROM ROLE
+                     WHERE role_name = 'Customer'
+                     """,
+                  )
+                  row = cur.fetchone()
+                  
+                  if row is None:
+                     raise ValueError("Role not found")
+                  
+                  role_id = str(row[0])
+
+                  # insert the user into user_account
+                  cur.execute(
+                     """
+                     INSERT INTO USER_ACCOUNT (user_id, username, password)
+                     VALUES (%s, %s, %s)
+                     """,
+                     (user_id, username, password),
+                  )
+                  
+                  # insert the user and it's role into account_role
+                  cur.execute(
+                     """ 
+                     INSERT INTO ACCOUNT_ROLE (role_id, user_id)
+                     VALUES (%s, %s)
+                     """,
+                     (role_id, user_id)
+                  )
+                  
+                  # insert the user into customer
+                  cur.execute(
+                     """ 
+                     INSERT INTO CUSTOMER (customer_id, full_name, phone_number, user_id)
+                     VALUES (%s, %s, %s, %s)
+                     """,
+                     (customer_id, full_name, nomor_telepon, user_id)
+                  )
+            conn.commit()
+            
+         except Exception:
+            # If any query fails, undo everything
+            conn.rollback()
+            raise
+         
+         finally:
+            conn.close()
+         
+      elif role == 'penyelenggara':
+         organizer_id = str(uuid.uuid4())
+         organizer_name = request.POST.get('nama_penyelenggara')
+         email = request.POST.get('email')
+         
+         try:
+            with conn.cursor() as cur:
+                  # get role id based on organizer
+                  cur.execute(
+                     """
+                     SELECT role_id
+                     FROM ROLE
+                     WHERE role_name = 'Organizer'
+                     """,
+                  )
+                  row = cur.fetchone()
+                  
+                  if row is None:
+                     raise ValueError("Role not found")
+                  
+                  role_id = str(row[0])
+
+                  # insert the user into user_account
+                  cur.execute(
+                     """
+                     INSERT INTO USER_ACCOUNT (user_id, username, password)
+                     VALUES (%s, %s, %s)
+                     """,
+                     (user_id, username, password),
+                  )
+                  
+                  # insert the user and it's role into account_role
+                  cur.execute(
+                     """ 
+                     INSERT INTO ACCOUNT_ROLE
+                     VALUES (%s, %s)
+                     """,
+                     (role_id, user_id)
+                  )
+                  
+                  # insert the user into organizer
+                  cur.execute(
+                     """ 
+                     INSERT INTO ORGANIZER
+                     VALUES (%s, %s, %s, %s)
+                     """,
+                     (organizer_id, organizer_name, email, user_id)
+                  )
+            conn.commit()
+            
+         except Exception:
+            # If any query fails, undo everything
+            conn.rollback()
+            raise
+         
+         finally:
+            conn.close()
+         
         
    return redirect('autentikasi:login')
--- basdut_adventure/settings.py
+++ basdut_adventure/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgqbg!3uwrtq2ga#v5jzcg=cecird0thb$bf_t-%o6qg4e2(19'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'main',
     'dashboard',
     'artist',
@@ -92,6 +95,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # Password validation
