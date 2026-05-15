9d69fe03aea05b4671ea73595df3879df5618427
Rainmo0 <aunknown145@gmail.com>
2026-05-15 10:37:20 +0700

    [feat] backend CR-Order

--- promotion/urls.py
+++ promotion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import show_promotions, dummy_promo_action
+from .views import show_promotions, dummy_promo_action, create_promotion
 
 app_name = 'promotion'
 
 urlpatterns = [
     path('', show_promotions, name='show_promotions'),
-    path('action/', dummy_promo_action, name='promo_action')
+    path('action/', dummy_promo_action, name='promo_action'),
+    path('action/create', create_promotion, name='create_promo')
 ]
--- promotion/views.py
+++ promotion/views.py
@@ -1,84 +1,74 @@
 from django.shortcuts import render, redirect
-
-# Data Dummy Promosi
-DUMMY_PROMOS = [
-    {
-        "id": "1d5afa98-bc36-42dd-b562-c10207e7d9cb",
-        "code": "FESTIVE10",
-        "type": "PERCENTAGE",
-        "value": "10%",  # Diformat string agar langsung siap tampil di HTML
-        "start_date": "2026-07-01",
-        "end_date": "2026-07-31",
-        "usage_count": 0, # Tambahan dummy untuk melengkapi tabel HTML
-        "max_usage": 100,
-        "icon_color": "purple" # Tambahan untuk style UI
-    },
-    {
-        "id": "0467f315-a328-4295-a317-7df5d669e234",
-        "code": "EARLY50",
-        "type": "NOMINAL",
-        "value": "Rp 50,000",
-        "start_date": "2026-07-05",
-        "end_date": "2026-07-20",
-        "usage_count": 12,
-        "max_usage": 50,
-        "icon_color": "orange"
-    },
-    {
-        "id": "ab5412af-73bb-467c-be3b-167cbb6718cb",
-        "code": "VIP20",
-        "type": "PERCENTAGE",
-        "value": "20%",
-        "start_date": "2026-07-10",
-        "end_date": "2026-08-10",
-        "usage_count": 28,
-        "max_usage": 30,
-        "icon_color": "purple"
-    },
-    {
-        "id": "ce3400b9-39c4-4496-bdc6-27c622c218d1",
-        "code": "SAVE25K",
-        "type": "NOMINAL",
-        "value": "Rp 25,000",
-        "start_date": "2026-08-01",
-        "end_date": "2026-08-31",
-        "usage_count": 45,
-        "max_usage": 80,
-        "icon_color": "orange"
-    },
-    {
-        "id": "3d5d748b-3b39-4ef9-b6bd-90f5d35d2f9f",
-        "code": "FLASH15",
-        "type": "PERCENTAGE",
-        "value": "15%",
-        "start_date": "2026-07-15",
-        "end_date": "2026-07-18",
-        "usage_count": 40,
-        "max_usage": 40,
-        "icon_color": "purple"
-    },
-    {
-        "id": "156c26de-b259-4924-9118-264fdde9efc3",
-        "code": "MEGA100K",
-        "type": "NOMINAL",
-        "value": "Rp 100,000",
-        "start_date": "2026-08-05",
-        "end_date": "2026-09-05",
-        "usage_count": 5,
-        "max_usage": 20,
-        "icon_color": "orange"
-    }
-]
+from django.contrib import messages
+from basdut_adventure.db import get_connection
+import uuid
 
 def show_promotions(request):
-    user_role = "ADMIN" 
+    user_role = request.session.get('role')
     
-    total_promos = len(DUMMY_PROMOS)
-    total_usage = sum(promo["usage_count"] for promo in DUMMY_PROMOS)
-    percentage_types = sum(1 for promo in DUMMY_PROMOS if promo["type"] == "PERCENTAGE")
+    # Getting all the promo data needed
+    
+    conn = get_connection()
+    
+    try:
+        with conn.cursor() as cur:
+                cur.execute(
+                """
+                SELECT
+                    p.promotion_id,
+                    p.promo_code,
+                    p.discount_type,
+                    p.discount_value,
+                    p.start_date,
+                    p.end_date,
+                    p.usage_limit,
+                    COUNT(op.promotion_id) AS total_usage
+                FROM promotion as p
+                LEFT JOIN order_promotion as op
+                    ON p.promotion_id = op.promotion_id
+                GROUP BY
+                    p.promotion_id,
+                    p.promo_code,
+                    p.discount_type,
+                    p.discount_value,
+                    p.start_date,
+                    p.end_date,
+                    p.usage_limit;
+                """
+                )
+                rows = cur.fetchall()
+                
+                promos = []
+
+                for row in rows:
+                    promo = {
+                        'id': row[0],          # promotion_id
+                        'code': row[1],        # promo_code
+                        'type': row[2],        # discount_type
+                        'value': row[3],       # discount_value
+                        'sdate': row[4],       # start_date
+                        'edate': row[5],       # end_date
+                        'max': row[6],         # usage_limit
+                        'used': row[7]         # total_usage
+                    }
+                    promos.append(promo)
+        conn.commit()
+        
+    except Exception:
+        # If any query fails, undo everything
+        messages.error(request, 'Uh oh! Something is wrong.')
+        conn.rollback()
+        raise
+    
+    finally:
+        conn.close()
+    
+    total_promos = len(promos)
+    total_usage = sum(promo["used"] for promo in promos)
+    percentage_types = sum(1 for promo in promos if promo["type"] == "PERCENTAGE")
 
     context = {
-        "promos": DUMMY_PROMOS,
+        "promos": promos,
         "user_role": user_role,
         "total_promos": total_promos,
         "total_usage": f"{total_usage}x",
@@ -89,3 +79,50 @@ def show_promotions(request):
 
 def dummy_promo_action(request):
     return redirect('promotion:show_promotions')
+
+def create_promotion(request):
+    if request.method == "POST" and request.session.get('role') == 'Admin':
+        kode_promo = request.POST.get("promo_code")
+        tipe_promo = request.POST.get("promo_type")
+        nominal_diskon = request.POST.get("discount_value")
+        tanggal_awal = request.POST.get("start_date")
+        tanggal_akhir = request.POST.get("end_date")
+        batas_penggunaan = request.POST.get("max_usage")
+        promo_id = str(uuid.uuid4())
+        
+        conn = get_connection()
+        
+        try:
+            with conn.cursor() as cur:
+                  cur.execute(
+                    """
+                    INSERT INTO promotion 
+                    (promotion_id, 
+                    promo_code, 
+                    discount_type, 
+                    discount_value, 
+                    start_date, 
+                    end_date, 
+                    usage_limit)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    """,
+                    [promo_id, kode_promo, tipe_promo, nominal_diskon, tanggal_awal, tanggal_akhir, batas_penggunaan]
+                  )
+
+            conn.commit()
+            messages.success(request, 'The promotion has been created.')
+            return redirect('promotion:show_promotions')
+            
+        except Exception as e:
+            # If any query fails, undo everything
+            conn.rollback()
+            messages.error(request, 'Uh oh! Something is wrong.')
+            raise
+        
+        finally:
+            conn.close()
+            
+    
+    messages.error(request, 'Uh oh! Something is wrong.')
+    return redirect('promotion:show_promotions')
+        
